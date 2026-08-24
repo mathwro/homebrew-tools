3598bd98d502ccb7b9fc5790b9a07a2896ff5799
Mathias Wrobel <mathias@wrobel.dk>
2026-08-24 14:19:35 +0200

    chore(azc): update to 0.2.0 (#14)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Formula/azc.rb
+++ Formula/azc.rb
@@ -5,23 +5,23 @@ class Azc < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/mathwro/azc/releases/download/v0.1.0/azc_0.1.0_darwin_arm64.tar.gz"
-      sha256 "c8a1e0d695b5b722c6184baf997270a29bb4861d2c050d68b9102adc6d761d65"
+      url "https://github.com/mathwro/azc/releases/download/v0.2.0/azc_0.2.0_darwin_arm64.tar.gz"
+      sha256 "29288bbaee28b5979fd79e5df04bb59fab9d47c0fc2917f65a7f9a6eb70542d4"
     end
     on_intel do
-      url "https://github.com/mathwro/azc/releases/download/v0.1.0/azc_0.1.0_darwin_amd64.tar.gz"
-      sha256 "cb2ab9ca3d154f69e5a0815c88b0ecd1e6a97ab1d4c8b6114c68a57a6aa7a40b"
+      url "https://github.com/mathwro/azc/releases/download/v0.2.0/azc_0.2.0_darwin_amd64.tar.gz"
+      sha256 "b1431cb18f848e69a0f35e6e74d360b496775995650a81d9b30e72cf66881481"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/mathwro/azc/releases/download/v0.1.0/azc_0.1.0_linux_arm64.tar.gz"
-      sha256 "c93d6b8f03749243e67e142c16fb49bb90d5abad287215faac565486c1e33701"
+      url "https://github.com/mathwro/azc/releases/download/v0.2.0/azc_0.2.0_linux_arm64.tar.gz"
+      sha256 "9078bb2a3cae63d54ce5a86c2e8fb76b1bd9d8e0b3b068bfa5036d592013a0ea"
     end
     on_intel do
-      url "https://github.com/mathwro/azc/releases/download/v0.1.0/azc_0.1.0_linux_amd64.tar.gz"
-      sha256 "02b431ee5cd492ee7daa8c6e8cab60c0b5d23dcf294f134f02384adc558b16ea"
+      url "https://github.com/mathwro/azc/releases/download/v0.2.0/azc_0.2.0_linux_amd64.tar.gz"
+      sha256 "99907c7c4cb070fdaf691f4c49cf21f9f908f87974d111461e4ab72acf18880b"
     end
   end
 
